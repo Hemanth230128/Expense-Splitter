1989d6796f742c358a1853c79ebf3b8bed09ef6e
hemanth ampili <hemanthampili2006@gmail.com>
2026-05-07 20:12:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2249162980-2e342",
+  "appId": "1:341657069062:web:30ed43c442aa81ca004587",
+  "apiKey": "AIzaSyBG-QSlUklDzPDab2PwXhMC79oNPyBYbCE",
+  "authDomain": "studio-2249162980-2e342.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "341657069062"
+};
